750d2378859e5073fec04762644cbbd0d9511a80
LalehRanjbaran <37128062+LalehRanjbaran@users.noreply.github.com>
2018-05-01 18:08:51 -0500

    Add files via upload

---
+++ driver.java
@@ -0,0 +1,20 @@
+package finalProject;
+
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class driver {
+
+	public static void main(String[] args) throws Exception {
+		
+		JFrame application =new frame3();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				application.setVisible(true);
+				application.setTitle("Tender");
+	}
+
+}
